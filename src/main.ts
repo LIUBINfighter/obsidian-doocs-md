7b03e185896afa7c23a8b1b6f13d3fbf06f967f7
JayBridge <12310903@mail.sustech.edu.cn>
2025-03-26 10:52:39 +0800

    重命名插件为 dooc-md，更新相关描述和配置，添加 TypeScript 支持，增加设置选项和命令功能

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface DoocsMdSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: DoocsMdSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DoocsMd extends Plugin {
+	settings: DoocsMdSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'dooc-md', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: DoocsMd;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DoocsMd) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
